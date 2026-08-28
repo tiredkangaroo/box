54179037cf89355c040e93625197ce8ee3a1b453
Ajitesh Kumar <ajinest6@gmail.com>
2026-08-28 19:03:35 -0400

    list mailboxes on nav

---
+++ frontend/src/lib/types.ts
@@ -0,0 +1,7 @@
+// get mailboxes
+export interface Mailbox {
+  id: string;
+  server_hostport: string;
+  username: string;
+  primary_inbox: string;
+}
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,13 +1,44 @@
 <script lang="ts">
   import "./layout.css";
+  import type { Mailbox } from "$lib/types";
   import favicon from "$lib/assets/favicon.svg";
 
   let { children } = $props();
+  let mailboxes = $state(undefined);
+
+  async function getMailboxes() {
+    const resp = await fetch("/api/mailboxes");
+    return await resp.json();
+  }
+  getMailboxes().then((data) => {
+    mailboxes = data;
+  });
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <nav>
-  <h1>box</h1>
-  <a role="button" href="/mailboxes">Manage Mailboxes</a>
+  <div>
+    <h1 class="text-center">box</h1>
+    <h2>Mailboxes</h2>
+    {#if mailboxes != undefined}
+      <div id="nav-mailboxes-list">
+        {#each mailboxes as Array<Mailbox> as mailbox}
+          <button
+            class="nav-mailbox"
+            onclick={(e) => {
+              (e.target as HTMLButtonElement).classList.toggle("active");
+            }}>{mailbox.username}</button
+          >
+        {/each}
+        <button
+          class="nav-mailbox"
+          onclick={(e) => {
+            (e.target as HTMLButtonElement).classList.toggle("active");
+          }}>meow</button
+        >
+      </div>
+    {/if}
+  </div>
+  <a role="button" href="/mailboxes" id="manage-mailboxes-button">Manage Mailboxes</a>
 </nav>
 {@render children()}
