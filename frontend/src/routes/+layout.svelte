fafac2b5feb60cdc7ba101d546bfc3a55b14eba0
Ajitesh Kumar <ajinest6@gmail.com>
2026-08-28 17:35:31 -0400

    starting frontend

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import "./layout.css";
+  import favicon from "$lib/assets/favicon.svg";
+
+  let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<nav>
+  <h1>box</h1>
+  <a role="button" href="/mailboxes">Manage Mailboxes</a>
+</nav>
+{@render children()}
